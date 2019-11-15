7b5cca6048605c9db99a97b72f4905f6c4cd1453
DFHG <54982245+DFHG10@users.noreply.github.com>
2019-11-15 22:09:43 +0800

    Update cpgx.java

--- coding-rear/cpgx.java
+++ coding-rear/cpgx.java
@@ -1,17 +1,21 @@
-package com.example.test2;
+package com.example.FZU;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class cpgx extends AppCompatActivity {
-
+    private Button btn1,btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cpgx);
+        btn1 = (Button)findViewById(R.id.btn_1);
+        btn2=(Button) findViewById(R.id.btn_2);
     }
     public void showToast(View view){
         Toast.makeText(this,"更新成功",Toast.LENGTH_SHORT).show();
