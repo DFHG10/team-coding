6a89cb48354de6864e2363e558108d8f5887135e
DFHG <54982245+DFHG10@users.noreply.github.com>
2019-11-15 22:09:28 +0800

    Create Fragment_meishi.java

---
+++ coding-rear/Fragment_meishi.java
@@ -0,0 +1,40 @@
+package com.example.FZU;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class Fragment_meishi extends Fragment {
+
+    private FragmentMeishiViewModel mViewModel;
+
+    public static Fragment_meishi newInstance() {
+        return new Fragment_meishi();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_meishi_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(FragmentMeishiViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
