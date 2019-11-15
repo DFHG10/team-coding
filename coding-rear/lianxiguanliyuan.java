3b05dc5c09688657125429577c6c6a06ad85949c
DFHG <54982245+DFHG10@users.noreply.github.com>
2019-11-15 22:08:58 +0800

    Update lianxiguanliyuan.java

--- coding-rear/lianxiguanliyuan.java
+++ coding-rear/lianxiguanliyuan.java
@@ -1,14 +1,32 @@
-package com.example.test2;
+package com.example.FZU;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 public class lianxiguanliyuan extends AppCompatActivity {
 
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lianxiguanliyuan);
+
+        btn = (Button)findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //跳转到register演示界面
+                Intent intent = new Intent(lianxiguanliyuan.this,fankuishangjiayemian.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
+
