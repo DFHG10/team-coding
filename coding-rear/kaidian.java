474b7e297534f1df2a0f27e68f6c1c3c407cc2fe
DFHG <54982245+DFHG10@users.noreply.github.com>
2019-11-15 22:08:36 +0800

    Update kaidian.java

--- coding-rear/kaidian.java
+++ coding-rear/kaidian.java
@@ -1,22 +1,37 @@
-package com.example.test2;
+package com.example.FZU;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class kaidian extends AppCompatActivity {
+    private Button mBtnToast1,mBtnToast2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kaidian);
+        mBtnToast1=(Button) findViewById(R.id.button_toast_1);
+        mBtnToast2=(Button) findViewById(R.id.button_toast_2);
+        OnClick onClick=new OnClick();
+        mBtnToast1.setOnClickListener(onClick);
+        mBtnToast2.setOnClickListener(onClick);
     }
-    public void showToast(View view){
-        Toast.makeText(this,"上传成功",Toast.LENGTH_SHORT).show();
+    class OnClick implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.button_toast_1:
+                    Toast.makeText(getApplicationContext(), "上传成功", Toast.LENGTH_LONG).show();
+                    break;
+                case R.id.button_toast_2:
+                    Toast.makeText(getApplicationContext(), "申请成功", Toast.LENGTH_LONG).show();
+                    break;
+            }
+        }
     }
-    public void showToast1(View view){
-        Toast.makeText(this,"申请成功",Toast.LENGTH_SHORT).show();
-    }
+
 }
